d14df834e2657304976ed750ae05f47b83d21bb6
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2017-10-21 13:19:19 +0800

    Trie

--- code/solve.cpp
+++ code/solve.cpp
@@ -5,24 +5,47 @@
 #include <algorithm>
 #define DEBUG(...) fprintf(stderr, __VA_ARGS__)
 
-const int N = 25;
-const int M = 15;
-const int BUFSZ = 256;
-
 using std::vector;
 using std::map;
 
-//typedef Hint std::vector<int>;
-struct Hint
-{
-    vector<int> cnts;
-    int idx;
-    bool isRow;
-};
+const int N = 25;
+const int M = 15;
+const int BUFSZ = 256;
+const int MEMSZ = 600000;
+
+std::vector<int> perm(M);
 
 inline int bit(int x, int i) { return x & (1 << i); }
+// inline int transform(int idx, int n) { return (idx & 1) ? (n - 1 - (idx >> 1)) : (idx >> 1); }
+// inline int transform(int idx, int n) { return n - idx - 1; }
+inline int transform(int idx, int n) { return idx; }
+// inline int transform(int idx, int n) { return perm[idx]; }
 
-map<vector<int>, vector<int>> db;
+struct Node
+{
+    Node *l, *r;
+    Node() : l(nullptr), r(nullptr) {}
+} mem[MEMSZ];
+int memCnt;
+
+Node *getNode()
+{
+    return &mem[memCnt++];
+}
+
+void insert(Node *&root, int val, int idx)
+{
+    if (!root) root = getNode();
+    if (idx >= M)
+        return ;
+    int bidx = transform(idx, M);
+    if (bit(val, bidx) == 0)
+        insert(root->l, val, idx+1);
+    else
+        insert(root->r, val, idx+1);
+}
+
+map<vector<int>, Node*> db;
 
 void gen_db()
 {
@@ -32,7 +55,7 @@ void gen_db()
     {
         vector<int> cnts;
         int count = 0;
-        for (int i = M - 1; i >= 0; i--)
+        for (int i = 0; i < M; i++)
         {
             if (bit(s, i))
                 count++;
@@ -43,7 +66,9 @@ void gen_db()
             }
         }
         if (count > 0) cnts.push_back(count);
-        db[std::move(cnts)].push_back(s);
+        if (db.find(cnts) == db.end())
+            db[cnts] = nullptr;
+        insert(db[cnts], s, 0);
     }
 }
 
@@ -53,114 +78,58 @@ class Solver
 {
     int n;
     Color (*board)[N];
-    Hint *hints;
+    vector<int> *vhints, *hhints;
+    Node *htries[N], *vtries[N];
 
-    void get_stat_and_mask(Color *ptr, int delta, int &stat, int &mask)
+    bool dfs(int ri, int ci)
     {
-        stat = mask = 0;
-        //DEBUG("-----\n");
-        for (int i = 0; i < n; i++)
+        if (ci == n)
         {
-            //DEBUG("%d ", *ptr);
-            stat <<= 1;
-            mask <<= 1;
-            if (*ptr != NONE)
-            {
-                mask |= 1;
-                if (*ptr == BLACK)
-                    stat |= 1;
-            }
-            //DEBUG("%d, %d\n", stat, mask);
-            ptr += delta;
+            ++ri;
+            ci = 0;
         }
-        //DEBUG("----\n");
-    }
-
-    void set(Color *ptr, int delta, int row)
-    {
-        for (int i = n - 1; i >= 0; i--)
+        if (ri == n) return true;
+        int r = transform(ri, n), c = transform(ci, n);
+        Node* htrie = htries[r];
+        Node* vtrie = vtries[c];
+        if (htrie->l && vtrie->l)
         {
-            *ptr = bit(row, i) ? BLACK : WHITE;
-            ptr += delta;
+            htries[r] = htrie->l;
+            vtries[c] = vtrie->l;
+            board[r][c] = WHITE;
+            if (dfs(ri, ci+1))
+                return true;
+            htries[r] = htrie;
+            vtries[c] = vtrie;
         }
-    }
-
-    void reset(Color *ptr, int delta, int stat, int mask)
-    {
-        for (int i = n - 1; i >= 0; i--)
+        if (htrie->r && vtrie->r)
         {
-            if (bit(mask, i))
-                *ptr = bit(stat, i) ? BLACK : WHITE;
-            else
-                *ptr = NONE;
-            ptr += delta;
+            htries[r] = htrie->r;
+            vtries[c] = vtrie->r;
+            board[r][c] = BLACK;
+            if (dfs(ri, ci+1))
+                return true;
+            htries[r] = htrie;
+            vtries[c] = vtrie;
         }
-    }
-
-    bool dfs(int i)
-    {
-        //DEBUG("dfs(%d):\n", i);
-        if (i >= n * 2)
-            return true;
-        Hint &h = hints[i];
-        int stat, mask;
-        Color *ptr;
-        int delta;
-        if (h.isRow)
-        {
-            ptr = &board[h.idx][0];
-            delta = 1;
-        }
-        else
-        {
-            ptr = &board[0][h.idx];
-            delta = N;
-        }
-        //DEBUG("%c %d\n", h.isRow ? 'r' : 'c', h.idx);
-        get_stat_and_mask(ptr, delta, stat, mask);
-        //DEBUG("stat = %d, mask = %d\n", stat, mask);
-        for (const auto &row: db[h.cnts])
-        {
-            //DEBUG("row = %d\n", row);
-            if (((row ^ stat) & mask) == 0)
-            {
-                set(ptr, delta, row);
-                //print_board(stderr);
-                if (dfs(i+1))
-                    return true;
-            }
-        }
-        reset(ptr, delta, stat, mask);
         return false;
     }
 
 public:
-    Solver(int _n, Hint *_hints)//, Color _board[][N])
-        : n(_n), hints(_hints)//, vhints(_vhints) //, board(_board)
+    Solver(int _n, vector<int> *_vhints, vector<int> *_hhints)
+        : n(_n), vhints(_vhints), hhints(_hhints)
     {
-        //DEBUG("%p %p %p\n", &board[3][1], &board[3][2], &board[3][1]+1);
+        for (int i = 0; i < n; i++)
+            vtries[i] = db[vhints[i]];
+        for (int i = 0; i < n; i++)
+            htries[i] = db[hhints[i]];
     }
 
     void solve(Color _board[][N])
     {
-        std::sort(hints, hints+n*2,
-            [](const Hint &h1, const Hint &h2) {
-                int v1 = h1.isRow ? (h1.idx*2) : (h1.idx*2+1);
-                int v2 = h2.isRow ? (h2.idx*2) : (h2.idx*2+1);
-                return v1 < v2;
-                //return db[h1.cnts].size() < db[h2.cnts].size();
-            }
-        );
-        //DEBUG("\n");
-        /*for (int i = 0; i < n * 2; i++)
-        {
-            for (auto &c: hints[i].cnts)
-                DEBUG("%d ", c);
-            DEBUG("\n");
-        }*/
         board = _board;
         memset(board, NONE, sizeof(Color)*N*N);
-        dfs(0);
+        dfs(0, 0);
     }
 
     void print_board(FILE *f = stdout)
@@ -174,12 +143,14 @@ public:
     }
 };
 
-//vector<int> hints[N*2];
-Hint hints[N*2];
+vector<int> hints[N*2];
 Color board[N][N];
 
 int main()
 {
+    for (int i = 0; i < M; i++)
+        perm[i] = i;
+    std::random_shuffle(perm.begin(), perm.end());
     gen_db();
     char buf[BUFSZ], *r;
     r = fgets(buf, 1024, stdin);
@@ -192,41 +163,24 @@ int main()
         int n = 0;
         while ((r = fgets(buf, 1024, stdin)) && buf[0] != '$')
         {
-            hints[n].cnts.clear();
+            hints[n].clear();
             for (char *ptr = buf; *ptr != '\n'; ptr++)
             {
                 int val = 0;
                 for (; *ptr >= '0' && *ptr <= '9'; ptr++)
                     val = val * 10 + *ptr - '0';
-                hints[n].cnts.push_back(val);
+                hints[n].push_back(val);
                 if (*ptr == '\n')
                     break;
             }
             ++n;
         }
         n >>= 1;
-        for (int i = 0; i < n; i++)
-        {
-            hints[i].idx = i;
-            hints[i].isRow = false;
-        }
-        for (int i = 0; i < n; i++)
-        {
-            hints[i+n].idx = i;
-            hints[i+n].isRow = true;
-        }
-        Solver s(n, hints);
+        Solver s(n, hints, hints+n);
         s.solve(board);
         s.print_board();
         DEBUG(" Done.\n");
-        /*for (int i = n - 1; i >= 0; i--)
-        {
-            for (int j = 0; j < n -1; j++)
-                printf("%d\t", board[i][j]-1);
-            printf("%d\n", board[i][n-1]-1);
-        }
         fflush(stdout);
-        break;*/
     }
     return 0;
 }
